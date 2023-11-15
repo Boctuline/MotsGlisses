c423c010e134130afe33e9d1a1adefb1bff650e1
Shire <shire2004ft@gmail.com>
2023-11-16 00:24:37 +0100

    Création d'une classe "Dictionnaire"

---
+++ Main/Dictionnaire.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MotsGlisses
+{
+    public class Dictionnaire
+    {
+        //Attributs
+        //Constructeur
+        public Dictionnaire()
+        {
+        }
+        //Méthodes
+        public string toString()
+        {
+            return null;
+        }
+        public bool RechDichRecursif(string mot)
+        {
+            return false;
+        }
+        public void Tri_Fusion()
+        {
+
+        }
+    }
+}
