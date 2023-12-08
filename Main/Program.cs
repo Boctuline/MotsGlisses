4d17864c345eee4bd4e5d11217a56a9d9f443710
Shire <shire2004ft@gmail.com>
2023-12-08 15:20:12 +0100

    update choix

--- Main/Program.cs
+++ Main/Program.cs
@@ -6,22 +6,29 @@ namespace MotsGlisses
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Comment voulez-vous ouvrir le plateau ?");
-
-            Plateau p = new Plateau();
+            Console.WriteLine("Comment voulez-vous ouvrir le plateau ?" +
+                "\n1. Plateau généré aléatoirement" +
+                "\n2. Plateau à partir d'un fichier");
+            string rep;
+            Plateau p = null;
+            do
+            {
+                rep = Console.ReadLine();
+                if (rep == "1") p = new Plateau();
+                if (rep == "2") p = new Plateau("..\\..\\..\\Main\\Fichiers\\Test1.csv");
+                else Console.WriteLine("Réponse incorrecte");
+            } while (rep != "1" || rep != "2");
             p.Afficher();
-            Plateau p1 = new Plateau("..\\..\\..\\Main\\Fichiers\\Test1.csv");
-            p1.Afficher();
             Joueur j1 = new Joueur("Shire");
             DateTime dt = DateTime.Now;
             while (true)
             {
-               string rep = Console.ReadLine();
-               List<Case> cases = p1.Recherche_Mot(rep);
+               rep = Console.ReadLine();
+               List<Case> cases = p.Recherche_Mot(rep);
                 if (cases == null) Console.WriteLine("Le mot n'est pas dans le tableau");
                 else
                 {
-                    p1.Maj_Plateau(new Cases(cases));
+                    p.Maj_Plateau(new Cases(cases));
                     Console.WriteLine(j1.Score(rep));
                     //Add score
                     //Add mot
